237a5cb8fd1092795c7c2646c93ac90816372acd
Khyati Sharma <khyatisharma57@gmail.com>
2020-05-12 18:16:47 +0530

    first commit

---
+++ src/items/items.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Param, Post, Body, Put, Delete, UseGuards } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from '../item';
+import { Items } from '../items';
+import { AuthGuard } from '@nestjs/passport';
+import { Permissions } from '../permissions.decorator';
+import { PermissionsGuard } from '../permissions.guard';
+
+@Controller('items')
+export class ItemsController {
+
+    constructor(private readonly itemsService: ItemsService) { }
+
+    @Get()
+    async findAll(): Promise<Items> {
+        return this.itemsService.findAll();
+    }
+
+    @Get(':id')
+    async find(@Param('id') id: number): Promise<Item> {
+        return this.itemsService.find(id);
+    }
+
+    @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+    @Post()
+    @Permissions('create:items')
+    create(@Body('item') item: Item) {
+        this.itemsService.create(item);
+    }
+
+    @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+    @Put()
+    @Permissions('update:items')
+    update(@Body('item') item: Item) {
+        this.itemsService.update(item);
+    }
+
+    @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+    @Delete(':id')
+    @Permissions('delete:items')
+    delete(@Param('id') id: number) {
+        this.itemsService.delete(id);
+    }
+}
